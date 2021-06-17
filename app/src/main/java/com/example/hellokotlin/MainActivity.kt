96a8deee446e1314f471c36a66c7f306e1626de9
Andrey Kadatsky <andrey.kadatsky@gmail.com>
2021-06-17 15:50:19 +0300

    Initial commit

---
+++ app/src/main/java/com/example/hellokotlin/MainActivity.kt
@@ -0,0 +1,21 @@
+package com.example.hellokotlin
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val button = findViewById<Button>(R.id.myButon)
+        val textView = findViewById<TextView>(R.id.myTextView)
+
+        button.setOnClickListener {
+            textView.text = "Andrey"
+        }
+
+    }
+}
